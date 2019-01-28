9c696e428f21427e6e3ea71996af9cf9490b4324
Joe Thaxton <14jthaxton@gmail.com>
2019-01-28 00:12:55 -0800

    vespene collector

--- ai.py
+++ ai.py
@@ -5,16 +5,19 @@ from sc2.constants import *
 
 
 class JoeBot(sc2.BotAI):
-    def __init(self):
-        self.drone_counter = 12
+    def __init__(self):
+        self.drone_counter = 14
+        self.spawning_pool = False 
+        self.extractor_started = False 
+        self.metabolic_boost_started = False 
 
     async def build_workers(self):
-        if self.can_afford(DRONE) & self.drone_counter < 14:
+        if self.can_afford(DRONE) and self.drone_counter < 16:
             await self.do(self.units(LARVA).random.train(DRONE))
             self.drone_counter += 1
 
     async def spawn_overlord(self):
-            if self.can_afford(OVERLORD) & self.supply_left < 2:
+            if self.can_afford(OVERLORD) and self.supply_left < 2:
                 await self.do(self.units(LARVA).random.train(OVERLORD))
 
     async def expand(self):
@@ -22,34 +25,48 @@ class JoeBot(sc2.BotAI):
             await self.expand_now()
             self.drone_counter -= 1 
 
-    async def build_assimilator(self):
-        for nexus in self.units(NEXUS).ready:
-            vespenes = self.state.vespene_geyser.closer_than(25.0, nexus)
-            for vespene in vespenes:
-                if not self.can_afford(ASSIMILATOR):
-                    break
-                worker = self.select_build_worker(vespene.position)
-                if worker is None:
-                    break 
-                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
-                    await self.do(worker.build(ASSIMILATOR,vespene))
+    async def build_extractor(self):
+        if not self.extractor_started:
+            if self.can_afford(EXTRACTOR):
+                drone = self.workers.random
+                target = self.state.vespene_geyser.closest_to(drone.position)
+                err = await self.do(drone.build(EXTRACTOR, target))
+                if not err:
                     self.drone_counter -= 1 
+                    self.extractor_started = True
         
     async def spawn_zergling(self):
         if self.can_afford(ZERGLING):
             await self.do(self.units(LARVA).random.train(ZERGLING))
 
+    async def build_spawning_pool(self):
+        if self.can_afford(SPAWNINGPOOL):
+            for d in range(4,15):
+                pos = self.units(HATCHERY).ready.first.position.to2.towards(self.game_info.map_center, d)
+                if await self.can_place(SPAWNINGPOOL, pos):
+                    drone = self.workers.closest_to(pos)
+                    err = await self.do(drone.build(SPAWNINGPOOL, pos))
+                    if not err:
+                        self.spawning_pool = True 
+                        break
+    async def upgrade_zerg_speed(self):
+        if self.can_afford(METABOLICBOOST) and self.metabolic_boost_started == False and self.extractor_started == True:
+            await self.do(self.units(SPAWNINGPOOL).ready.first(RESEARCH_ZERGLINGMETABOLICBOOST))
+            self.metabolic_boost_started = True 
 
     async def on_step(self,iteration):
         if iteration == 0:
             await self.chat_send("GLHF")
-
+        if self.drone_counter > 12 and self.spawning_pool == True and self.units(LARVA).exists:
+            await self.spawn_zergling()
         await self.distribute_workers()
-        await self.build_workers()
-        await self.spawn_overlord()
+        if self.spawning_pool == False:
+            await self.build_spawning_pool()
+        if self.units(LARVA).exists:
+            await self.build_workers()
+            await self.spawn_overlord()
         await self.expand()
-        await self.build_assimilator()
-        await self.spawn_zergling()
+        await self.build_extractor()
 
 
 
